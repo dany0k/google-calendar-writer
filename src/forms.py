81605a46012fc00d85c154f733565195ecb8b722
Den-Laptop <zmaev.vsu@gmail.com>
2023-04-02 14:12:22 +0300

    Add new subgroup field

--- src/forms.py
+++ src/forms.py
@@ -1,21 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import widgets, StringField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
-class SubmitForm(FlaskForm):
+class SubmitScheduleForm(FlaskForm):
     submit = SubmitField(label="Submit")
     group_number = StringField(
         label='Номер группы',
         validators=[
             DataRequired(),
-            Length(max=20)
+            Length(max=2)
         ]
     )
     course_number = StringField(
         label='Номер курса',
         validators=[
             DataRequired(),
-            Length(max=20)
+            Length(max=2)
+        ]
+    )
+    subgroup_number = StringField(
+        label='Номер подгруппы',
+        validators=[
+            DataRequired(),
+            Length(max=2)
         ]
     )
