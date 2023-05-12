51c5d64c943408ef513484c21ec232c777aae627
DenPC <zmaev.vsu@gmail.com>
2023-05-12 22:25:38 +0300

    Update js

--- src/static/js/loading.js
+++ src/static/js/loading.js
@@ -4,16 +4,33 @@ document.addEventListener("DOMContentLoaded", function () {
      // Получаем форму
      var form = document.querySelector("form");
      var preloader = document.getElementById("preloader")
-
      // Добавляем обработчик события нажатия на кнопку отправки формы
      submitBtn.addEventListener("click", function () {
-         preloader.style.display = "block";
-         form.submit();
+        if (validateForm()) {
+            preloader.style.display = "block";
+            form.submit();
+        }
      });
  });
+
  document.addEventListener("DOMContentLoaded", function () {
      // Получаем элемент с классом "loader"
      var preloader = document.getElementById("preloader")
-     // Скрываем элемент с классом "loader"
      preloader.style.display = "none";
+     // Скрываем элемент с классом "loader"
  });
+
+ function validateForm() {
+    const course = document.getElementById("course_num_id"); 
+    const group = document.getElementById("group_num_id"); 
+    const subgroup = document.getElementById("subgroup_num_id"); 
+    const week = document.getElementById("week_num_id"); 
+    
+    if (course.value === '' ||
+        group.value === '' ||
+        subgroup.value === '' ||
+        week.value === '') {
+        return true;
+    }
+    return false;
+}
